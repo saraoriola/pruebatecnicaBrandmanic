5886cdf5edde81ba511e459ebfe4a81af5a01c03
saraoriola <saraoriola@outlook.com>
2023-10-03 19:29:01 +0200

    Nav

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './Header/Header'
+import { Box } from '@chakra-ui/react'
 
 const Dashboard = () => {
   return (
-    <>
-    <Header/>
-    </>
+    <Box bg="#014B54">
+      <Box bg={"#f2f2f2"} h="100vh" ml="100px"  borderStartRadius={50}> 
+            <Header/>      
+      </Box>
+    </Box>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,48 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import React from 'react';
+import { Box, Flex, Input, InputGroup, InputRightElement, Button, Avatar, Text, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { SearchIcon, ChevronDownIcon } from '@chakra-ui/icons';
 
 const Header = () => {
   return (
-    <Box>
-
-        
+    <Box p={7}>
+      <Flex alignItems="center" justifyContent="space-between">
+        <InputGroup width={500}>
+          <Input
+          height={10}
+            type="text"
+            placeholder="Buscar"
+            borderRadius={20}
+            bgColor="#f2f2f2"
+            _placeholder={{ color: 'gray.400' }}
+            boxShadow="inset 0 0 8px rgba(0, 0, 0, 0.1)"
+          />
+          <InputRightElement width="3rem">
+            <Button
+              h="1.75rem"
+              size="sm"
+              onClick={() => {}}
+              color={"#014B54"}
+            >
+              <SearchIcon />
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Flex alignItems="center">
+          <Avatar size="sm" name="S O" src="URL de la imagen" mr={3} />
+          <Menu>
+            <MenuList>
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>Settings</MenuItem>
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} variant="link" color="#014B54">
+              <Text fontWeight="bold" color="#014B54">Sara Oriola</Text>
+            </MenuButton>
+          </Menu>
+        </Flex>
+      </Flex>
     </Box>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
